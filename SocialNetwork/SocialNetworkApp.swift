fafec730650fcf7f1c602d73e7990809a40eb027
AndresGutierrez <c0dingw.dre@gmail.com>
2022-05-05 08:26:28 -0700

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Andres Gutierrez on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
